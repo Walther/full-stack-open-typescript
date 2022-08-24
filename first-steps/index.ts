696464473268951ba3e81056e31013e928834ad5
Walther <veeti.haapsamo@gmail.com>
2022-08-24 13:51:19 +0300

    Solve exercises 9.4 - 9.5

---
+++ first-steps/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { calculateBmi } from "./bmiCalculator";
+const app = express();
+
+app.get("/ping", (_req, res) => {
+  res.send("pong");
+});
+
+app.get("/bmi", (req, res) => {
+  const params = req.query;
+  const weight: number = Number(params.weight);
+  if (isNaN(weight)) {
+    res.status(400).send({ error: "malformatted parameters" });
+    return;
+  }
+  const height: number = Number(params.height);
+  if (isNaN(height)) {
+    res.status(400).send({ error: "malformatted parameters" });
+    return;
+  }
+  const bmi = calculateBmi(height, weight);
+  res.send({ weight, height, bmi });
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

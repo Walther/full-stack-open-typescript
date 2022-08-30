85de9e3f4053595d51058dbd7351f2ee039cea8e
Walther <veeti.haapsamo@gmail.com>
2022-08-30 12:02:21 +0300

    Solve exercise 9.7

--- first-steps/index.ts
+++ first-steps/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { calculateBmi } from "./bmiCalculator";
+import bodyParser from "body-parser";
+import { calculateExercises } from "./exerciseCalculator";
 const app = express();
+app.use(bodyParser.json());
 
 app.get("/ping", (_req, res) => {
   res.send("pong");
@@ -20,6 +23,32 @@ app.get("/bmi", (req, res) => {
   }
   const bmi = calculateBmi(height, weight);
   res.send({ weight, height, bmi });
+});
+
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { target, daily_exercises } = req.body;
+  const parsed_target = Number(target);
+
+  if (isNaN(parsed_target)) {
+    res.status(400).send({ error: "malformatted target" });
+    return;
+  }
+
+  const parsed_daily_exercises = [];
+  for (const hour of daily_exercises) {
+    const parsed_hour = Number(hour);
+    if (isNaN(parsed_hour)) {
+      res.status(400).send({ error: "malformatted daily exercises" });
+      return;
+    }
+    parsed_daily_exercises.push(parsed_hour);
+  }
+  const exerciseAnalysis = calculateExercises(
+    parsed_target,
+    parsed_daily_exercises
+  );
+  res.send(exerciseAnalysis);
 });
 
 const PORT = 3003;
